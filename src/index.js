05a16a409b23381964bf04350ab08c58396f411c
Artsiom Rak <ra44elo@gmail.com>
2019-03-13 19:59:06 +0300

    feat: add implementation of the function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  // Your code goes here!
+  // Return an object containing the minimum number of coins needed to make change
+  let nominals = ['H', 'Q', 'D', 'N', 'P'];
+  let coins = [50, 25, 10, 5, 1];
+  let exchanged = {};
+
+  for(let i = 0; i < coins.length; i++) {
+    if(currency == 0 || currency < 0) {
+      return exchanged;
+    }
+    else if(currency > 10000) {
+      return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    else if (currency / coins[i] >= 1) {
+      let remainder = Math.floor(currency / coins[i]);
+      currency = currency - remainder * coins[i];
+      exchanged[nominals[i]] = remainder;
+    }
+    else continue;
+  }
+  return exchanged;
 }
